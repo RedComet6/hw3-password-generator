524637fcb2d01bccf3f85d85f5747bdd5179ab15
Aidan Chamberlain <asunshine99@gmail.com>
2021-12-21 22:05:10 -0600

    array additions, length check

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // 1b. create array of letters
 let lcLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"];
+let ucLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"];
+let pwNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specChars = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "*",
+  "+",
+  "-",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "^",
+  "_",
+  "~",
+];
 
 // generatePassword
 // return a final password as a string
-
 function generatePassword() {
   // 1. assign a variable to the length of the password
-  let pwLength = prompt("How long of a password would you like?");
+  let pwLength = prompt(
+    "Passwords should be between 8 and 128 chracters long. How long of a password would you like?"
+  );
+
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Please choose a password length between 8 and 128 characters");
+    generatePassword();
+  }
+
+  let useLower = confirm("Do you want lowercase letters to be used?");
+  let useUpper = confirm("Do you want upper case letters to be used?");
+  let useNumbers = confirm("Do you want numbers to be used?");
+  let useSpec = confirm("Do you want special characters to be used?");
+
   let password = [];
 
-  // 1a. create a password with that length
-
   // 1c. loop through array of letters
-  for (let i = 0; i < pwLength; i++) {
-    password.push(lcLetters[i]);
+  if (useLower === true) {
+    for (let i = 0; i < pwLength; i++) {
+      password.push(lcLetters[i]);
+    }
   }
 
   // 1d. push each letter array into password array
   // 1e. create a random 10 letter password
-  // 2 ask user for length
 
   // 1e. convert password array back to string
   return password.join("");
